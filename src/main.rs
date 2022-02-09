d88a07edb1f148ec7af695746c1d72699aac8a66
jkcoxson <jkcoxson@gmail.com>
2022-02-09 08:47:04 -0700

    Add basic dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////
+///   Vision Processing for the ALC Robotics Team    ///
+/// Jackson Coxson, Josh Something, Wesley Something ///
+///                     2022                         ///
+////////////////////////////////////////////////////////
+
+
+
+
 fn main() {
     println!("Hello, world!");
 }
