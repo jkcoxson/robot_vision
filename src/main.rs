3a1f5fed034a4a07f3e06417e23df3d4e4bc3c2b
jkcoxson <jkcoxson@gmail.com>
2022-02-12 13:56:11 -0700

    Make move

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,20 @@
 ///                     2022                         ///
 ////////////////////////////////////////////////////////
 
+use opencv::prelude::*;
+use opencv::videoio::VideoCapture;
 
-
-
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let mut cam = VideoCapture::new(0, opencv::videoio::CAP_ANY).unwrap();
+    opencv::highgui::named_window("your mom", opencv::highgui::WINDOW_AUTOSIZE).unwrap();
+    let mut frame = Mat::default();
+    loop {
+        cam.read(&mut frame).unwrap();
+        opencv::highgui::imshow("your mom", &frame).unwrap();
+        let key = opencv::highgui::wait_key(1).unwrap();
+        if key == 27 {
+            break;
+        }
+    }
 }
