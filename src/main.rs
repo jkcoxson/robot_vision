e9a4f01b7369beecf9b74b6ae2dbc455f1575471
jkcoxson <jkcoxson@gmail.com>
2022-02-22 08:41:22 -0700

    Add basic networking

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,33 @@
 
 use opencv::prelude::*;
 use opencv::videoio::VideoCapture;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let mut cam = VideoCapture::new(0, opencv::videoio::CAP_ANY).unwrap();
-    opencv::highgui::named_window("your mom", opencv::highgui::WINDOW_AUTOSIZE).unwrap();
-    let mut frame = Mat::default();
-    loop {
-        cam.read(&mut frame).unwrap();
-        opencv::highgui::imshow("your mom", &frame).unwrap();
-        let key = opencv::highgui::wait_key(1).unwrap();
-        if key == 27 {
-            break;
+    tokio::spawn( async {
+        let mut cam = VideoCapture::new(0, opencv::videoio::CAP_ANY).unwrap();
+        opencv::highgui::named_window("your mom", opencv::highgui::WINDOW_AUTOSIZE).unwrap();
+        let mut frame = Mat::default();
+        loop {
+            cam.read(&mut frame).unwrap();
+            match opencv::highgui::imshow("your mom", &frame) {
+                Ok(()) => {},
+                Err(e) => println!("{}", e)
+            }
+            let key = opencv::highgui::wait_key(1).unwrap();
+            if key == 27 {
+                break;
+            }
         }
+    });
+    
+
+    // Create a TCP listener on port 6969
+    let listener = TcpListener::bind("0.0.0.0:6969").await.unwrap();
+    loop {
+        // Accept a new connection
+        let (_, _) = listener.accept().await.unwrap();
+        println!("Got a connection!");
     }
 }
---
+++ src/robot.rs
@@ -0,0 +1,13 @@
+
+
+pub struct Robot {
+    pub sender: UnboundedSender<Message>,
+}
+
+impl Robot {
+    pub fn new() -> Robot {
+        Robot {
+
+        }
+    }
+}
